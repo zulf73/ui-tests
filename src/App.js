813723ec3b889678605bacac123c7404416f9655
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-07 10:44:30 -0600

    image display works for avatar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {Avatar} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import zulf_lit from "./zulf_lit.jpg";
+const useStyles = makeStyles(theme => ({
+  large: {
+    width: theme.spacing(8),
+    height: theme.spacing(8)
+  }
+}));
 
+//<Avatar alt="Zulf Ahmed" src="https://photos.google.com/share/AF1QipMMAQV4FeAtzYOFFDyOF2ANqj9RpsSvhfSERtPt8fNwdvWrRRCzxHVygCrje05Hzg/photo/AF1QipP7K1egE2x_HY9vQkr0fccxc6WacaCGdcc5WWeH?key=eF9KcHE2akhtaEw1QUtyNHZ3aUxfZVozaTVBcjRB" />
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const classes = useStyles();
+    return (
+	  <div>
+	  <Avatar alt="Zulf"
+	src={zulf_lit}
+	className={classes.large}
+	    />
+	    
+	    <img src={zulf_lit} />
+</div>
   );
 }
 
